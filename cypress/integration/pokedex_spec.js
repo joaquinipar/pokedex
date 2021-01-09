b338a7809239d29561fc3f606ca8c2a88ce102d6
Joaquin Ipar <joaquin.ipar@gmail.com>
2021-01-08 21:57:16 -0300

    Add items and stats feature. Cypress test added

--- cypress/integration/pokedex_spec.js
+++ cypress/integration/pokedex_spec.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-// import * as typeahead from '.node_modules\\corejs-typeahead\\dist\\typeahead.bundle.min.js';
 const URL = 'localhost:8080';
 const POKEMON_NAME = 'pikachu';
 
@@ -9,14 +8,37 @@ describe('Pokedex', () => {
     cy.wait(100);
   });
   it('search a valid pokemon and get a response displayed', () => {
-    cy.get('#selected-pokemon').type(POKEMON_NAME);
+    cy.get('#selected-pokemon').click({ force: true }).type(POKEMON_NAME);
     cy.get('#search-pokemon').click({ force: true });
     cy.get('#pokemon-name').contains(POKEMON_NAME);
   });
   it('search an non existing pokemon and get an alert and a red border', () => {
-    cy.get('#selected-pokemon').type('mario bros');
+    cy.get('#selected-pokemon').click({ force: true }).type('mario bros');
     cy.get('#search-pokemon').click({ force: true });
     cy.get('.alert-danger').should('have.css', 'visibility', 'visible');
     cy.get('#selected-pokemon').should('have.css', 'borderColor', 'rgb(255, 0, 0)');
+  });
+  it('language changes succesfully when selected from the dropdown bar', () => {
+    const englishPosition = 1;
+    const spanishPosition = 2;
+    const spanishWelcomeMessage = 'Ingresa tu pokemon preferido!';
+    const englishWelcomeMessage = 'Type your favorite Pokemon!';
+    cy.get('.language-select').click();
+    cy.get('.language-select').eq(spanishPosition).click();
+    cy.get('#welcome').contains(spanishWelcomeMessage);
+    cy.get('.language-select').eq(0).click();
+    cy.get('.language-select').eq(englishPosition).click();
+    cy.get('#welcome').contains(englishWelcomeMessage);
+  });
+  it('select second pokemon from the dropdown menu and get a response displayed', () => {
+    let pokemonSelectedName;
+    cy.get('#selected-pokemon').click({ force: true }).type(POKEMON_NAME.slice(0, 3)).blur();
+    cy.wait(100);
+    cy.get('.tt-selectable').eq(1).then((selectable) => {
+      pokemonSelectedName = selectable.text();
+      cy.get('.tt-selectable').eq(1).click({ force: true });
+      cy.get('#search-pokemon').click({ force: true });
+      cy.get('#pokemon-name').contains(pokemonSelectedName);
+    });
   });
 });
--- pokedex.js
+++ pokedex.js
@@ -10,9 +10,23 @@ const $pokemonBaseXP = document.querySelector('#pokemon-base-xp');
 const $pokemonWeight = document.querySelector('#pokemon-weight');
 const $errorAlert = document.querySelector('.alert-danger');
 const $pokemonSearch = document.querySelector('#selected-pokemon');
+const $pokemonItems = document.querySelector('#pokemon-item-list');
+const $pokemonHP = document.querySelector('#pokemon-hp');
+const $pokemonAttack = document.querySelector('#pokemon-attack');
+const $pokemonDefense = document.querySelector('#pokemon-defense');
+const $pokemonSpecialAttack = document.querySelector('#pokemon-special-attack');
+const $pokemonSpeed = document.querySelector('#pokemon-speed');
+const pokemonStatsElements = [$pokemonHP, $pokemonAttack, $pokemonDefense,
+  $pokemonSpecialAttack, $pokemonSpeed];
 let languageNumber = 7;
 
 function clearAbilities() { $pokemonAbilities.innerHTML = ''; }
+
+async function fetchInfo(URL) {
+  const response = await fetch(URL);
+  const responseJSON = await response.json();
+  return responseJSON;
+}
 
 async function specificPokemonData(pokemonName) {
   const response = await fetch(specificPokemon + pokemonName);
@@ -35,6 +49,11 @@ function changeLanguage(newLanguage) {
     $('#pokemon-base-xp-h3').text('Experiencia Base');
     $('#pokemon-weight-h3').text('Peso');
     $($errorAlert).text('Asegurese de ingresar un pokemon válido.');
+    // $("#pokemon-hp-h3") remains unchanged
+    $('#pokemon-attack-h3').text('Ataque');
+    $('#pokemon-defense-h3').text('Defensa');
+    $('#pokemon-special-attack-h3').text('Ataque Especial');
+    $('#pokemon-speed-h3').text('Velocidad');
     languageNumber = 5;
   } else if (newLanguage === 'English') {
     $('#welcome').text('Type your favorite Pokemon!');
@@ -46,6 +65,11 @@ function changeLanguage(newLanguage) {
     $('#pokemon-base-xp-h3').text('Base XP');
     $('#pokemon-weight-h3').text('Weight');
     $($errorAlert).text('Make sure to enter a valid pokemon.');
+    // $("#pokemon-hp-h3") remains unchanged
+    $('#pokemon-attack-h3').text('Attack');
+    $('#pokemon-defense-h3').text('Defense');
+    $('#pokemon-special-attack-h3').text('Special Attack');
+    $('#pokemon-speed-h3').text('Speed');
     languageNumber = 7;
   }
   $('#search-pokemon').click(); // reload search in the new language
@@ -57,17 +81,44 @@ function updatePokemonInfo(element, newText) {
 function updatePokemonImg(element, newSrc) {
   element.src = newSrc;
 }
+function updatePokemonItems(element, itemList) {
+  itemList.forEach((itemObject) => {
+    console.log(itemObject.item.url);
+    fetchInfo(itemObject.item.url).then((item) => {
+      const itemName = item.names[languageNumber].name;
+      const itemImg = item.sprites.default;
+      console.log(`nombre del item: ${itemName}`);
+      console.log(`imagen del item ${itemImg}`);
+
+      const newRow = $('<div/>').addClass('row');
+      const newDiv1 = $('<div/>').addClass('col-sm-6');
+      const newImg = $('<img/>').attr('src', itemImg);
+      const newDiv2 = $('<div/>').addClass('col-sm-6');
+      const newItemText = $('</p>').text(itemName);
+
+      newDiv1.append(newImg);
+      newDiv2.append(newItemText);
+      newRow.append(newDiv1);
+      newRow.append(newDiv2);
+      $(element).append(newRow);
+    })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
+
+function updatePokemonStats(statsList) {
+  pokemonStatsElements.forEach((element, index) => {
+    element.textContent = statsList[index].base_stat;
+  });
+}
 
 function updateAbilities(element, abilitiesList) {
   abilitiesList.forEach((abilityObject) => {
-    console.log(abilityObject);
-    console.log(`url: ${abilityObject.ability.url}`);
-
     fetch(abilityObject.ability.url)
       .then((response) => response.json())
       .then((responseJSON) => {
-        console.log(responseJSON.names);
-        console.log(responseJSON.names[languageNumber].name);
         $(element).append($('<li/>').text(responseJSON.names[languageNumber].name).attr('class', 'list-group-item'));
       });
   });
@@ -107,6 +158,14 @@ function loadSearchEngine() {
     });
 }
 
+function clearStatsInfo() {
+  updatePokemonInfo($pokemonHP, '');
+  updatePokemonInfo($pokemonAttack, '');
+  updatePokemonInfo($pokemonDefense, '');
+  updatePokemonInfo($pokemonSpecialAttack, '');
+  updatePokemonInfo($pokemonSpeed, '');
+}
+
 function clearPokemonInfo() {
   updatePokemonInfo($pokemonName, '');
   updatePokemonInfo($pokemonBaseXP, '');
@@ -125,6 +184,9 @@ function clearAlert() {
   $errorAlert.style.visibility = 'hidden';
   $pokemonSearch.style.borderColor = '';
 }
+function clearItem() {
+  $pokemonItems.innerHTML = '';
+}
 
 setTimeout(() => {
   $('.bootstrap-select').on('click', (e) => {
@@ -142,10 +204,11 @@ $('#myList a').on('click', function (e) {
 $('#search-pokemon').on('click', (e) => {
   e.preventDefault();
   clearAbilities();
+  clearStatsInfo();
   clearPokemonInfo();
   clearPokemonImg();
   clearAlert();
-  console.log(` ${$('#selected-pokemon')[0].value} `);
+  clearItem();
   const selectedPokemon = $('#selected-pokemon')[0].value;
 
   specificPokemonData(selectedPokemon).then((pokemonJSON) => {
@@ -154,6 +217,8 @@ $('#search-pokemon').on('click', (e) => {
     updatePokemonInfo($pokemonWeight, pokemonJSON.weight);
     updatePokemonImg($pokemonImg, pokemonJSON.sprites.front_default);
     updateAbilities($pokemonAbilities, pokemonJSON.abilities);
+    updatePokemonItems($pokemonItems, pokemonJSON.held_items);
+    updatePokemonStats(pokemonJSON.stats);
   })
     .catch(() => {
       toggleFailure();
